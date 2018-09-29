de780675d50a3b66460942a3258034e2fe731299
Nicolas Rodriguez <nicoladmin@free.fr>
2018-09-29 05:11:41 +0200

    Remove useless dependency on jquery-rails

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -1,8 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-require "jquery-rails"
-require 'coffee-rails'
+# require "jquery-rails"
+# require 'coffee-rails'
 require 'bootstrap-sass'
 
 # Initialize the Rails application.
